2151e912662f2e8be805a22e06ce1e1ee38c5f3b
bslgw <54250945+bslgw@users.noreply.github.com>
2026-05-27 06:23:35 +0800

    Update config.js

--- htdocs/luci-static/resources/view/singbox/config.js
+++ htdocs/luci-static/resources/view/singbox/config.js
@@ -6,23 +6,26 @@
 
 return L.view.extend({
 	render: function() {
+		// 顯式要求加載這兩個模組
 		var luci_form = L.require('form');
 		var luci_fs = L.require('fs');
 
 		return Promise.all([luci_form, luci_fs]).then(L.bind(function(modules) {
-			var form = modules;
-			var fs = modules;
+			// 修正點：從陣列中精確提取模組
+			// modules[0] 對應 luci_form, modules[1] 對應 luci_fs
+			var form = modules[0];
+			var fs = modules[1];
+			
 			var m, s, o;
 
-			m = new form.Map('sing-box', _('Sing-box Bridge'), 
-				_('專為 daed 配套設計的內核管理工具。'));
+			m = new form.Map('sing-box', _('Sing-box Bridge'), _('專為 daed 配套設計的內核管理工具。'));
 
 			// --- 1. 內核狀態 ---
 			s = m.section(form.TypedSection, '_info', _('內核狀態'));
 			s.anonymous = true;
 			s.render = L.bind(function() {
 				return fs.exec('/usr/bin/sing-box', ['version']).then(function(res) {
-					var ver_info = (res.code === 0) ? res.stdout.split('\n') : _('未安裝');
+					var ver_info = (res.code === 0) ? res.stdout.split('\n')[0] : _('未安裝');
 					return E('div', { 'class': 'cbi-value' }, [
 						E('label', { 'class': 'cbi-value-title' }, _('版本資訊')),
 						E('div', { 'class': 'cbi-value-field' }, [
@@ -41,13 +44,11 @@ return L.view.extend({
 
 			o = s.option(form.Value, 'conffile', _('主配置文件路徑'));
 			o.placeholder = '/etc/sing-box/config.json';
-			o.description = _('Sing-box 啟動時讀取的 JSON 檔案位置');
 
-			// --- 3. 節點配置 (根據你的建議簡化) ---
+			// --- 3. 節點配置 ---
 			s = m.section(form.GridSection, 'node', _('節點配置'));
 			s.addremove = true;
 			s.nodescriptions = true;
-			// 如果沒數據，LuCI 預設會顯示 "Table is empty"，這很直觀
 
 			o = s.option(form.Value, 'name', _('名稱'));
 			o.placeholder = _('例如：香港節點');
@@ -56,7 +57,6 @@ return L.view.extend({
 			o.rows = 15;
 			o.wrap = 'off';
 			o.modalonly = true;
-			o.description = _('請在此處貼入該節點的 JSON 配置。');
 
 			return m.render();
 		}, this));
