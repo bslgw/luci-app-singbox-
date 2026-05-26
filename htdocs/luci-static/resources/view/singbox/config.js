a36f0737c22e525a95574e117a085506323d9725
bslgw <54250945+bslgw@users.noreply.github.com>
2026-05-27 05:55:25 +0800

    Update config.js

--- htdocs/luci-static/resources/view/singbox/config.js
+++ htdocs/luci-static/resources/view/singbox/config.js
@@ -3,59 +3,61 @@
 'import fs';
 'import view';
 'import form';
-'import uci';
 
 /*
- * 使用 L.view.extend 並顯式傳入模組，確保 form 和 fs 可用
+ * 使用 L.view.extend 並在內部顯式調用 L.require 確保模組可用
  */
 return L.view.extend({
 	render: function() {
-		// 顯式宣告，防止模組未定義
-		var m, s, o;
+		// 關鍵修正：不再依賴頂層變數，直接從 L 命名空間中獲取模組
+		var luci_form = L.require('form');
+		var luci_fs = L.require('fs');
 
-		m = new form.Map('sing-box', _('Sing-box Bridge'), _('輕量級內核管理工具，專為 daed 配套設計。'));
+		return Promise.all([luci_form, luci_fs]).then(L.bind(function(modules) {
+			var form = modules[0];
+			var fs = modules[1];
+			var m, s, o;
 
-		// --- 1. 內核資訊 ---
-		s = m.section(form.TypedSection, '_info', _('內核資訊'));
-		s.anonymous = true;
-		s.render = L.bind(function() {
-			return fs.exec('/usr/bin/sing-box', ['version']).then(function(res) {
-				var ver = (res.code === 0) ? res.stdout.split('\n')[0] : _('未安裝或無法執行');
-				return E('div', { 'class': 'cbi-value' }, [
-					E('label', { 'class': 'cbi-value-title' }, _('Version / Arch')),
-					E('div', { 'class': 'cbi-value-field' }, [
-						E('span', { 'class': 'label' }, ver + ' (' + L.env.arch + ')')
-					])
-				]);
-			}).catch(function(e) {
-				return E('div', { 'class': 'cbi-value' }, _('讀取版本失敗'));
-			});
-		}, this);
+			m = new form.Map('sing-box', _('Sing-box Bridge'), _('輕量級內核管理工具，專為 daed 配套設計。'));
 
-		// --- 2. 服務狀態 ---
-		s = m.section(form.NamedSection, 'main', 'singbox', _('服務狀態'));
-		s.addremove = false;
+			// --- 1. 內核資訊 ---
+			s = m.section(form.TypedSection, '_info', _('內核狀態'));
+			s.anonymous = true;
+			s.render = L.bind(function() {
+				return fs.exec('/usr/bin/sing-box', ['version']).then(function(res) {
+					var ver = (res.code === 0) ? res.stdout.split('\n')[0] : _('未安裝');
+					return E('div', { 'class': 'cbi-value' }, [
+						E('label', { 'class': 'cbi-value-title' }, _('版本 / 架構')),
+						E('div', { 'class': 'cbi-value-field' }, [
+							E('span', { 'class': 'label' }, ver + ' (' + L.env.arch + ')')
+						])
+					]);
+				});
+			}, this);
 
-		o = s.option(form.Flag, 'enabled', _('啟用服務'));
-		o.rmempty = false;
+			// --- 2. 服務控制 ---
+			s = m.section(form.NamedSection, 'main', 'singbox', _('服務開關'));
+			s.addremove = false;
+			o = s.option(form.Flag, 'enabled', _('啟用外置內核'));
+			o.rmempty = false;
 
-		o = s.option(form.Value, 'conffile', _('設定檔路徑'));
-		o.placeholder = '/etc/sing-box/config.json';
-		o.datatype = 'file';
+			o = s.option(form.Value, 'conffile', _('設定檔路徑'));
+			o.placeholder = '/etc/sing-box/config.json';
 
-		// --- 3. 節點管理 ---
-		s = m.section(form.GridSection, 'node', _('節點配置庫'));
-		s.addremove = true;
-		s.nodescriptions = true;
+			// --- 3. 節點管理 ---
+			s = m.section(form.GridSection, 'node', _('節點配置庫'), _('在這裡手動貼入 Outbound JSON 片段。'));
+			s.addremove = true;
+			s.nodescriptions = true;
 
-		o = s.option(form.Value, 'name', _('標籤'));
-		o.rmempty = false;
+			o = s.option(form.Value, 'name', _('節點標籤'));
+			o.rmempty = false;
 
-		o = s.option(form.TextValue, 'config_json', _('JSON 配置'));
-		o.rows = 15;
-		o.wrap = 'off';
-		o.modalonly = true;
+			o = s.option(form.TextValue, 'config_json', _('JSON 配置'));
+			o.rows = 15;
+			o.wrap = 'off';
+			o.modalonly = true;
 
-		return m.render();
+			return m.render();
+		}, this));
 	}
 });
