b9a6be26ee9487920a0e39be577798e44119a300
bslgw <54250945+bslgw@users.noreply.github.com>
2026-05-27 08:16:39 +0800

    Update config.js

--- htdocs/luci-static/resources/view/singbox/config.js
+++ htdocs/luci-static/resources/view/singbox/config.js
@@ -7,7 +7,7 @@
 'import poll';
 
 return L.view.extend({
-	// 秒顯示狀態：增加立即執行的邏輯
+	// 立即檢查狀態
 	checkStatus: function() {
 		return L.fs.exec('/etc/init.d/sing-box', ['status']).then(function(res) {
 			var isRunning = (res.code === 0);
@@ -22,7 +22,6 @@ return L.view.extend({
 	handleAction: function(action, filename, confdir) {
 		var target = confdir + '/config.json';
 		var source = confdir + '/' + filename;
-
 		if (action === 'switch') {
 			L.ui.showModal(_('應用配置'), [E('p', _('正在切換至 %s ...').format(filename))]);
 			return L.fs.exec('/bin/cp', [source, target]).then(function() {
@@ -37,7 +36,6 @@ return L.view.extend({
 		}
 	},
 
-	// 支援空檔案編輯：增加預設值處理
 	handleEdit: function(filename, confdir) {
 		var path = confdir + '/' + filename;
 		return L.fs.read(path).then(function(content) {
@@ -54,13 +52,9 @@ return L.view.extend({
 					])
 				])
 			]);
-		}).catch(function() { 
-			// 如果檔案不存在，則建立新檔案
-			return L.fs.write(path, '{}').then(function() { location.reload(); });
 		});
 	},
 
-	// 新建配置功能
 	handleCreate: function(confdir) {
 		var nameInput = E('input', { 'placeholder': 'new_config.json', 'style': 'width:100%' });
 		L.ui.showModal(_('新建配置文件'), [
@@ -80,23 +74,35 @@ return L.view.extend({
 	},
 
 	render: function() {
-		return Promise.all([L.require('form'), L.require('fs'), L.require('ui'), L.require('uci'), L.require('poll')]).then(L.bind(function(modules) {
-			var form = modules, fs = modules, ui = modules, uci = modules, poll = modules;
-			L.ui = ui; L.fs = fs;
+		// 強制精確加載
+		return Promise.all([
+			L.require('form'),
+			L.require('fs'),
+			L.require('ui'),
+			L.require('uci'),
+			L.require('poll')
+		]).then(L.bind(function(results) {
+			// 核心修正：手動建立變數映射，不使用任何簡寫
+			var form_mod = results[0];
+			var fs_mod = results[1];
+			var ui_mod = results[2];
+			var uci_mod = results[3];
+			var poll_mod = results[4];
 
-			var m = new form.Map('sing-box', _('Sing-box Bridge'), _('SING-BOX 服務管理'));
+			// 掛載全局 L
+			L.ui = ui_mod;
+			L.fs = fs_mod;
 
-			// 1. 狀態與路徑整合 (解決空間浪費)
-			var s = m.section(form.TypedSection, '_status', _('服務控制'));
+			var m = new form_mod.Map('sing-box', _('Sing-box Bridge'), _('SING-BOX 服務管理'));
+
+			var s = m.section(form_mod.TypedSection, '_status', _('服務控制'));
 			s.anonymous = true;
 			s.render = L.bind(function() {
-				var confdir = uci.get('sing-box', 'main', 'confdir') || '/etc/sing-box';
-				// 立即執行第一次狀態檢查
+				var confdir = uci_mod.get('sing-box', 'main', 'confdir') || '/etc/sing-box';
 				this.checkStatus();
-				poll.add(L.bind(this.checkStatus, this), 5);
-				
-				return E('div', { 'class': 'cbi-value', 'style': 'display:flex; align-items:center; border-bottom:1px solid #eee; padding-bottom:10px;' }, [
-					E('label', { 'class': 'cbi-value-title', 'style': 'margin-bottom:0; width:15%' }, _('運行狀態')),
+				poll_mod.add(L.bind(this.checkStatus, this), 5);
+				return E('div', { 'class': 'cbi-value', 'style': 'display:flex; align-items:center; border-bottom:1px solid #eee;' }, [
+					E('label', { 'class': 'cbi-value-title', 'style': 'width:15%' }, _('運行狀態')),
 					E('div', { 'class': 'cbi-value-field', 'style': 'width:85%; display:flex; align-items:center;' }, [
 						E('span', { 'id': 'sb_status_label', 'class': 'label', 'style': 'color:#fff; padding:4px 8px; border-radius:3px; background:#999;' }, _('檢測中...')),
 						E('strong', { 'style': 'margin-left:20px; color:#666;' }, _('工作目錄: ')),
@@ -106,19 +112,17 @@ return L.view.extend({
 				]);
 			}, this);
 
-			// 2. 列表管理 (按鈕一行化與新建功能)
-			s = m.section(form.TypedSection, '_list', _('可用配置文件'));
+			s = m.section(form_mod.TypedSection, '_list', _('可用配置文件'));
 			s.anonymous = true;
 			s.render = L.bind(function() {
-				var confdir = uci.get('sing-box', 'main', 'confdir') || '/etc/sing-box';
-				return fs.list(confdir).then(L.bind(function(files) {
+				var confdir = uci_mod.get('sing-box', 'main', 'confdir') || '/etc/sing-box';
+				return fs_mod.list(confdir).then(L.bind(function(files) {
 					var table = E('table', { 'class': 'table cbi-section-table' }, [
 						E('tr', { 'class': 'tr cbi-section-table-titles' }, [
 							E('th', { 'class': 'th' }, _('檔案名稱')),
 							E('th', { 'class': 'th', 'style': 'width:240px; text-align:center;' }, _('管理操作'))
 						])
 					]);
-
 					files.forEach(L.bind(function(file) {
 						if (file.name.endsWith('.json') && file.name !== 'config.json') {
 							table.appendChild(E('tr', { 'class': 'tr' }, [
@@ -131,14 +135,8 @@ return L.view.extend({
 							]));
 						}
 					}, this));
-
-					return E('div', {}, [
-						table,
-						E('div', { 'style': 'margin-top:10px' }, [
-							E('button', { 'class': 'cbi-button cbi-button-add', 'click': L.ui.createHandlerFn(this, 'handleCreate', confdir) }, _('＋ 新建配置文件'))
-						])
-					]);
-				}, this)).catch(function() { return E('div', { 'class': 'alert-message warning' }, _('目錄讀取失敗，請檢查 /etc/sing-box 是否存在。')); });
+					return E('div', {}, [ table, E('button', { 'class': 'cbi-button cbi-button-add', 'style': 'margin-top:10px;', 'click': L.ui.createHandlerFn(this, 'handleCreate', confdir) }, _('＋ 新建配置文件')) ]);
+				}, this)).catch(function() { return E('div', { 'class': 'alert-message warning' }, _('目錄讀取失敗。')); });
 			}, this);
 
 			return m.render();
