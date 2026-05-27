16efe8e975f5eab532de3710928938b9041d41d1
bslgw <54250945+bslgw@users.noreply.github.com>
2026-05-27 12:00:25 +0800

    Update config.js

--- htdocs/luci-static/resources/view/singbox/config.js
+++ htdocs/luci-static/resources/view/singbox/config.js
@@ -7,10 +7,13 @@
 'import poll';
 
 return L.view.extend({
-	// 強力重啟組合拳
+	// 真正的強力重啟邏輯
 	doRestart: function() {
-		return L.fs.exec('/etc/init.d/sing-box', ['stop']).then(function() {
-			return L.fs.exec('/etc/init.d/sing-box', ['start']);
+		return L.fs.exec('/etc/init.d/sing-box', ['restart']).then(function(res) {
+			// 如果 restart 沒反應，強行 stop + start
+			return L.fs.exec('/etc/init.d/sing-box', ['stop']).then(function() {
+				return L.fs.exec('/etc/init.d/sing-box', ['start']);
+			});
 		});
 	},
 
@@ -30,13 +33,11 @@ return L.view.extend({
 		var source = confdir + '/' + filename;
 		var btn = ev.target;
 		var oldText = btn.textContent;
-		
 		btn.disabled = true; btn.textContent = _('正在應用...'); btn.style.background = '#ffc107';
 
 		return L.fs.read(source).then(function(content) {
 			return L.fs.write(target, content || '{}');
 		}).then(L.bind(function() {
-			// 改用強力重啟
 			return this.doRestart();
 		}, this)).then(L.bind(function() {
 			btn.textContent = _('完成'); btn.style.background = '#28a745';
@@ -51,20 +52,17 @@ return L.view.extend({
 	},
 
 	render: function() {
-		return Promise.all([
-			L.require('ui'), L.require('fs'), L.require('form'), L.require('uci'), L.require('poll')
-		]).then(L.bind(function(res) {
-			var ui_mod = res[0], fs_mod = res[1], form_mod = res[2], uci_mod = res[3], poll_mod = res[4];
-			L.ui = ui_mod; L.fs = fs_mod;
+		return Promise.all([ L.require('ui'), L.require('fs'), L.require('form'), L.require('uci'), L.require('poll') ]).then(L.bind(function(res) {
+			var ui_m = res, fs_m = res, form_m = res, uci_m = res, poll_m = res;
+			L.ui = ui_m; L.fs = fs_m;
 
-			var m = new form_mod.Map('sing-box', _('Sing-box Bridge'), _('SING-BOX 服務管理'));
-
-			var s = m.section(form_mod.TypedSection, '_status', _('服務控制'));
+			var m = new form_m.Map('sing-box', _('Sing-box Bridge'), _('SING-BOX 服務管理'));
+			var s = m.section(form_m.TypedSection, '_status', _('服務控制'));
 			s.anonymous = true;
 			s.render = L.bind(function() {
-				var confdir = uci_mod.get('sing-box', 'main', 'confdir') || '/etc/sing-box';
+				var confdir = uci_m.get('sing-box', 'main', 'confdir') || '/etc/sing-box';
 				this.checkStatus();
-				poll_mod.add(L.bind(this.checkStatus, this), 5);
+				poll_m.add(L.bind(this.checkStatus, this), 5);
 				return E('div', { 'class': 'cbi-value', 'style': 'display:flex; align-items:center; border-bottom:1px solid #eee; padding-bottom:10px;' }, [
 					E('label', { 'class': 'cbi-value-title', 'style': 'width:15%' }, _('運行狀態')),
 					E('div', { 'class': 'cbi-value-field', 'style': 'width:85%; display:flex; align-items:center;' }, [
@@ -72,20 +70,20 @@ return L.view.extend({
 						E('strong', { 'style': 'margin-left:20px; color:#666;' }, _('目錄: ')),
 						E('span', { 'style': 'font-family:monospace; margin-left:5px;' }, confdir),
 						E('button', { 'class': 'cbi-button cbi-button-reset', 'style': 'margin-left:auto;', 'click': L.bind(function(ev) {
-							var b = ev.target; b.textContent = _('正在重啟...');
+							ev.target.textContent = _('正在重啟...');
 							return this.doRestart().then(L.bind(function(){
-								b.textContent = _('重啟服務'); this.checkStatus();
+								ev.target.textContent = _('重啟服務'); this.checkStatus();
 							}, this));
 						}, this) }, _('重啟服務'))
 					])
 				]);
 			}, this);
 
-			s = m.section(form_mod.TypedSection, '_list', _('可用配置文件'));
+			s = m.section(form_m.TypedSection, '_list', _('可用配置文件'));
 			s.anonymous = true;
 			s.render = L.bind(function() {
-				var confdir = uci_mod.get('sing-box', 'main', 'confdir') || '/etc/sing-box';
-				return fs_mod.list(confdir).then(L.bind(function(files) {
+				var confdir = uci_m.get('sing-box', 'main', 'confdir') || '/etc/sing-box';
+				return fs_m.list(confdir).then(L.bind(function(files) {
 					var table = E('table', { 'class': 'table cbi-section-table' }, [
 						E('tr', { 'class': 'tr cbi-section-table-titles' }, [
 							E('th', { 'class': 'th' }, _('檔案名稱')),
@@ -99,18 +97,18 @@ return L.view.extend({
 								E('td', { 'class': 'td', 'style': 'white-space:nowrap; text-align:center;' }, [
 									E('button', { 'class': 'btn cbi-button-apply', 'style': 'margin:0 2px;', 'click': L.bind(this.handleSwitch, this, file.name, confdir) }, _('選用')),
 									E('button', { 'class': 'btn cbi-button-neutral', 'style': 'margin:0 2px;', 'click': function() {
-										fs_mod.read(confdir + '/' + file.name).catch(function(){ return ''; }).then(function(c) {
+										fs_m.read(confdir + '/' + file.name).catch(function(){ return ''; }).then(function(c) {
 											var ta = E('textarea', { 'style': 'width:100%; height:400px; font-family:monospace;' }, [ c || '{}' ]);
-											ui_mod.showModal(_('編輯: %s').format(file.name), [ E('div', { 'style': 'padding:10px' }, [ ta, E('div', { 'class': 'right', 'style': 'margin-top:10px' }, [
-												E('button', { 'class': 'btn', 'click': ui_mod.hideModal }, _('取消')),
+											ui_m.showModal(_('編輯: %s').format(file.name), [ E('div', { 'style': 'padding:10px' }, [ ta, E('div', { 'class': 'right', 'style': 'margin-top:10px' }, [
+												E('button', { 'class': 'btn', 'click': ui_m.hideModal }, _('取消')),
 												E('button', { 'class': 'btn cbi-button-positive', 'style': 'margin-left:10px', 'click': function() {
-													fs_mod.write(confdir + '/' + file.name, ta.value).then(function() { ui_mod.hideModal(); });
+													fs_m.write(confdir + '/' + file.name, ta.value).then(function() { ui_m.hideModal(); });
 												}}, _('儲存'))
 											]) ]) ]);
 										});
 									} }, _('編輯')),
 									E('button', { 'class': 'btn cbi-button-remove', 'style': 'margin:0 2px;', 'click': function() {
-										if (confirm(_('刪除 %s？').format(file.name))) fs_mod.remove(confdir + '/' + file.name).then(function(){ location.reload(); });
+										if (confirm(_('刪除 %s？').format(file.name))) fs_m.remove(confdir + '/' + file.name).then(function(){ location.reload(); });
 									} }, _('刪除'))
 								])
 							]));
@@ -119,12 +117,11 @@ return L.view.extend({
 					return E('div', {}, [ 
 						table, 
 						E('button', { 'class': 'cbi-button cbi-button-add', 'style': 'margin-top:10px;', 'click': L.bind(function() {
-							var name = prompt(_('新檔名 (.json):'));
-							if (name && name.endsWith('.json')) {
-								// 關鍵修正：在寫入前先嘗試執行一次指令確保目錄可寫，並強制刷新頁面
-								return fs_mod.exec('/bin/touch', [confdir + '/' + name]).then(function() {
-									return fs_mod.write(confdir + '/' + name, '{}');
-								}).then(function() {
+							var name = prompt(_('請輸入新檔名:'));
+							if (name) {
+								if (!name.endsWith('.json')) name += '.json';
+								// 核心積累：使用 fs_m 直接寫入空 JSON，並強行 reload
+								return fs_m.write(confdir + '/' + name, '{}').then(function() {
 									location.reload();
 								}).catch(function(err) {
 									alert(_('建立失敗: ') + err);
